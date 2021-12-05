d33c741b8ee8d5102a44c1a927ea18ddf4888836
Dhruv Dave <200456157@student.georgianc.on.ca>
2021-12-04 19:26:59 -0500

    Added country-details-view.fxml and its controller

---
+++ src/main/java/com/example/javaassignment2/Controllers/CountryDetailsViewController.java
@@ -0,0 +1,4 @@
+package com.example.javaassignment2.Controllers;
+
+public class CountryDetailsViewController {
+}
--- src/main/java/com/example/javaassignment2/Controllers/CountryViewController.java
+++ src/main/java/com/example/javaassignment2/Controllers/CountryViewController.java
@@ -4,13 +4,16 @@ import com.example.javaassignment2.AllCountries;
 import com.example.javaassignment2.ApiResponse;
 import com.example.javaassignment2.Utilities.APIUtility;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CountryViewController {
+public class CountryViewController implements Initializable {
 
     @FXML
     private ListView<String> countryListView;
@@ -21,11 +24,18 @@ public class CountryViewController {
     @FXML
     private Label msgLabel;
 
-    @FXML
-    private void getCountryLoaded() throws IOException, InterruptedException {
-        ApiResponse apiResponse = APIUtility.getAllCountriesFromOMDB();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ApiResponse apiResponse = null;
+        try {
+            apiResponse = APIUtility.getAllCountriesFromOMDB();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         countryListView.getItems().addAll(apiResponse.getBody().getCountries());
     }
-
 }
